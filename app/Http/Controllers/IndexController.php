9a012a84f50c25c6519e23fb8559da52002f5f92
Abdullah <abdullahjumaalarja@gmail.com>
2024-10-03 14:30:38 +0300

    solve text font size

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $file = File::get(public_path('file13.json'));
+        $file = File::get(public_path('file14.json'));
         $json = json_decode($file, true);
         $html = Json2HtmlUnit::convert($json);
         return view('welcome', compact('html'));
